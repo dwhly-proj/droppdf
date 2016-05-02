91a8affdeaf77371f2c780ccefdf1fcb60a50a87
Alexey <alexeygolovin5587@gmail.com>
2016-05-02 10:28:02 -0700

    doc & cvs

--- upload/templatetags/custom_tags.py
+++ upload/templatetags/custom_tags.py
@@ -5,10 +5,9 @@ register = template.Library()
 @register.filter(name='format')
 def format(value):
 	res = value
-	if len(value) > 0 and value[len(value) - 1] != "\"":
-		res = value[1:-3]
-	elif len(value) > 0:
-		res = value[1:-1]
+	
+	if len(value) > 0:
+		res = value[1:]
 	return res
 
 
