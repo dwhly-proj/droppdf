e3fb65ae05fc9813e87f93afcd618ac2fd654f3b
paul <paul@darkstar.org>
2016-09-07 20:19:33 -0600

    comment handle missing configs

--- pdf_annotator/views.py
+++ pdf_annotator/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 
+#configs.py contains secrets that shouldn't be in the public repo
+#so handle it if the file isn't there... don't prevent the whole app from running
 try:
     from configs import CLIENT_ID, API_KEY, SCOPES  
 except ImportError:
