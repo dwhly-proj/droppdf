5b1e5a57943607311e7abdba3d0fbd36fa157469
paul <paul.genho@gmail.com>
2020-08-18 13:31:36 -0600

    position embed video and subs

--- upload/static/js/youtube.js
+++ upload/static/js/youtube.js
@@ -1,11 +1,24 @@
 $(document).ready(function(){
     var player;
 
+    var source = 'https://www.youtube.com/embed/'
+    source += window.videoId; 
+    source += '?enablejsapi=1'
+    //source += '?enablejsapi=1&origin='
+    //source += window.location; 
+    source += '&widgetid=1';
+
+    console.log(source);
+
+    $('#video-player-iframe').prop('src', source);
+
+    console.log($('#video-player-iframe'))
+
     function onYouTubeIframeAPIReady() {
-        player = new YT.Player('video-player', {
-            height: '390',
-            width: '640',
-            videoId: window.videoId, 
+        player = new YT.Player('video-player-iframe', {
+            //height: '390',
+            //width: '640',
+            //videoId: window.videoId, 
             events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
