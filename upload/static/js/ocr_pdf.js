ee00e0f514a264ce560f7552685749933fbf75a0
paul <paul.genho@gmail.com>
2021-01-29 13:06:40 -0700

    changes to ocr results page

--- upload/static/js/ocr_pdf.js
+++ upload/static/js/ocr_pdf.js
@@ -1,8 +1,11 @@
 (function() {
 
-    /* 5 minute max time */
-    var max = 60
+    /* 50 minute max time */
+    var max = 600
     var c = 0
+
+    var stop_time;
+    var start_time = new Date();
 
 
     var check_interval = setInterval(function() {
@@ -10,11 +13,17 @@
         .done(function(result) {
             clearInterval(check_interval);
 
+            stop_time = new Date();
+
             var download_link = '/static/drop-pdf/' + OCR_FILE_NAME
             var docdrop_link = '/pdf/' + OCR_FILE_NAME
 
+            var time_to_process = ((stop_time - start_time) / 1000).toFixed(2)
+
             $('#in-progress').hide();
             $('#download-info').show();
+
+            $('#processing-time').text(time_to_process);
 
             $('#file-docdrop-link').attr('href', '/pdf/' + OCR_FILE_NAME)
             $('#file-download-link').attr('href', '/static/drop-pdf/' + OCR_FILE_NAME)
@@ -38,6 +47,13 @@
     }, 5000);
 
     $(document).ready(function() {
+        $('#time-start').text(start_time.toLocaleTimeString());
+
+        var url = window.location.origin;
+        url += '/static/drop-pdf/' + OCR_FILE_NAME;
+
+        $('#download-link').text(url);
+
         if (PROCESSING_ERROR != 'None' || !PROCESSING_ERROR || PROCESSING_ERROR === '') {
 
             clearInterval(check_interval);
@@ -49,6 +65,5 @@
                 .show();
         }
     });
-
 
 }());
